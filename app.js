919fcb77a11264cd3dd0e2b3081153e4c7876fc0
haseebbhatti742 <haseebbhatti742@github.com>
2020-10-07 11:31:30 +0500

    API Structure updated

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
+//libraries
 const express = require('express');
-const app = express();
-const http = require('http');
+const app1 = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mysql = require('mysql');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const cors = require('cors');
+var passport = require('passport');
+//const passportSetup = require('');
+
+//settings
+app1.use(cookieParser('secret'));
+app1.use(flash());
+app1.use(bodyParser.json({ limit: '50mb' }));
+app1.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app1.use(cors());
+app1.use(bodyParser.json());
+
+app1.use(express.static('assets'));
+app1.set('views', './views');
+app1.set('view engine', 'jade');
+
+//session management
+app1.use(session({
+    saveUninitialized: false,
+    resave: true,
+    secret: 'ssshhhhh',
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }
+}));
+app1.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+app1.use(passport.initialize());
+app1.use(passport.session());
 
 //deifining routes
 const indexRoute = require("./routes/index");
 
-app.use("/", indexRoute);
+app1.use("/", indexRoute);
 
-module.exports = app;
+module.exports.app = app1;
--- server.js
+++ server.js
@@ -1,16 +1,9 @@
 const http = require('http');
-const express = require('express');
-const bodyParsed = require('body-parser');
-const app = require("./app");
+const app1 = require("./app");
+const app = app1.app
+
 const port = process.env.PORT || 8080;
 const server = http.createServer(app);
-
-app.use(express.static('assets'));
-app.set('views', './views');
-app.set('view engine', 'jade');
-app.use(bodyParsed.urlencoded({
-    extended: true
-}));
 
 server.listen(port, function() {
     console.log('Server Running! Open Chrome Now');
